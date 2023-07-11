7dbbe066c4ff1e99216f5b20d1ee9e8a8ee4d7ae
Jordana <jordanarmos@gmail.com>
2023-07-11 10:29:56 -0300

    feat: Ending AboutUs component

--- src/components/pages/AboutUs.jsx
+++ src/components/pages/AboutUs.jsx
@@ -1,6 +1,11 @@
 import { useContext } from 'react';
 import {
-    Background
+    Background,
+    TextBackground,
+    Title,
+    DatasGrid,
+    Datas,
+    Description
 } from '../styles/AboutUs.style';
 import { LanguageContext } from '../../language.context.js';
 
@@ -10,7 +15,31 @@ export default function AboutUs(){
 
     return(
         <Background>
-            
+            <TextBackground>
+                <Title>{translations.AboutUs.Title}</Title>
+
+                <DatasGrid>
+                    <Datas>
+                        <h3>93840K+</h3>
+                        <p>Sales</p>
+                    </Datas>
+                    <Datas>
+                        <h3>19231ETH+</h3>
+                        <p>Profit</p>
+                    </Datas>
+                    <Datas>
+                        <h3>346K+</h3>
+                        <p>Artist</p>
+                    </Datas>   
+                </DatasGrid>
+                
+            </TextBackground>
+            <Description>
+                Porttitor sagittis faucibus vel netus ultrices libero facilisi sit ipsum urna, tempor sem gravida enim, orci molestie amet dignissim justo, massa eu neque dictum at ut placerat nulla dignissim ut ornare enim ut interdum tincidunt at tincidunt.
+                <br/>
+                <br/>
+                Diam faucibus ac in hac justo fringilla molestie amet, lacus scelerisque condimentum adipiscing eros habitasse neque pulvinar integer massa ut dui diam amet eget varius lorem scelerisque orci blandit maecenas cursus fermentum neque quisque at est, semper eleifend eget eros blandit consectetur justo, lacus, iaculis mattis feugiat hendrerit quis felis pulvinar quis ornare.
+            </Description>
         </Background>
     )
 }
--- src/components/styles/AboutUs.style.js
+++ src/components/styles/AboutUs.style.js
@@ -1,6 +1,37 @@
 import styled from 'styled-components';
 
 export const Background = styled.section`
+    width: calc(100% - 140px);
+    background-image: linear-gradient(to left, #121212, #171717);
+    padding: 100px;
+    display: grid;
+    grid-template-columns: auto 55%;
+    align-items: center;
+`
+
+export const TextBackground = styled.div`
+    width: fit-content;
+    height: fit-content;
+`
+export const Title = styled.h1`
+    font-weight: 500;
+    font-size: 4rem;
+`
+export const DatasGrid = styled.div`
     width: 100%;
-    min-height: 500px;
+    display: flex;
+    gap: 10px;
+    align-items: center;
+    justify-content: space-between;
+`
+export const Datas = styled.span`
+    gap: 10px;
+    
+    p{
+        font-size: 14px;
+    }
+`
+
+export const Description = styled.p`
+    font-size: 16px;
 `
