f1999d7c1f8a692a83e4e240feff4ef94211dde5
Jordana <jordanarmos@gmail.com>
2023-07-11 03:31:17 -0300

    feat: Making Presentation component

--- src/components/pages/Presentation.js
+++ src/components/pages/Presentation.js
@@ -1,11 +1,37 @@
+import { useContext } from 'react';
 import {
-    Background
+    Background,
+    BullsImageGrid,
+    TextBackground,
+    StartingText,
+    Title,
+    Subtitle,
+    SeeMoreButton,
+    LetsGoButton
 } from '../styles/Presentation';
+import { LanguageContext } from '../../language.context.js';
+
+import Arrow from '../images/Presentation/Arrow.svg';
 
 export default function Presentation(){
+    const { translations } = useContext(LanguageContext);
+
     return(
         <Background>
-            
+            <BullsImageGrid/>
+            <TextBackground>
+                <StartingText>{translations.Presentation.ConnectWithUs}</StartingText>
+                <Title>{translations.Presentation.Title}</Title>
+                <Subtitle>{translations.Presentation.Subtitle}</Subtitle>
+
+                <div style={{display: 'flex', gap: '15px', marginTop: '30px'}}>
+                    <SeeMoreButton>{translations.Presentation.SeeMoreButton}</SeeMoreButton>
+                    <LetsGoButton>
+                        <img src={Arrow}/>
+                        {translations.Presentation.LetsGoButton}
+                    </LetsGoButton>
+                </div>
+            </TextBackground>
         </Background>
     )
 }
--- src/components/styles/Presentation.js
+++ src/components/styles/Presentation.js
@@ -1,7 +1,63 @@
 import styled from 'styled-components';
 
+import CryptoBullsImage from '../images/Presentation/CryptoBulls.png'
+
 export const Background = styled.section`
     width: 100%;
     min-height: 650px;
-    background-color: red;
+    display: grid;
+    grid-template-columns: 505px auto;
+`
+
+export const BullsImageGrid = styled.span`
+    background-image: url(${CryptoBullsImage});
+    margin-left: -95px;
+`
+
+export const TextBackground = styled.span`
+    width: fit-content;
+    heigth: fit-content;
+    margin: auto 0 auto auto;
+
+    & *{
+        width: fit-content;
+        margin-left: auto;
+    }
+`
+
+export const StartingText = styled.h2`
+    background: linear-gradient(to right, #B621FE, #F53803);
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+    text-transform: uppercase;
+    font-size: 16px;
+`
+export const Title = styled.h1`
+    width: 75%;
+    font-size: 40px;
+    letter-spacing: .5px;
+    text-align: end;
+    font-weight: 600;
+`
+export const Subtitle = styled.p`
+    font-size: 14px;
+    margin-top: 10px;
+`
+
+export const SeeMoreButton = styled.button`
+    color: #fff;
+    border: 1px solid white;
+    padding: 10px 15px;
+`
+export const LetsGoButton = styled(SeeMoreButton)`
+    background-color: white;
+    color: #121212;
+    display: flex;
+    gap: 10px;
+    align-items: center;
+
+    img{
+        width: 20px;
+        height: 20px;
+    }
 `
