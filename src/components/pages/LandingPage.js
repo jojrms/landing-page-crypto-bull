c8efb968c8c34a93ba26a45856383340a4fc60b1
Jordana <jordanarmos@gmail.com>
2023-07-11 02:47:45 -0300

    language: Landing Page makes the language change

--- src/components/pages/LandingPage.js
+++ src/components/pages/LandingPage.js
@@ -11,21 +11,22 @@ import LanguageIcon from '../images/Header/Language.svg'
 
 export default function LandingPage(){
 
-    const { language, changeLanguage } = useContext(LanguageContext);
+    const { translations, language, changeLanguage } = useContext(LanguageContext);
 
     return(
         <>
             <Header>
                 <Icon/>
                 <Ul>
-                    <li>Discover</li>
-                    <li>About Us</li>
-                    <li>Community</li>
-                    <li>Market</li>
-                    <li>Suport</li>
+                    <li>{translations.Header.Discover}</li>
+                    <li>{translations.Header.AboutUs}</li>
+                    <li>{translations.Header.Community}</li>
+                    <li>{translations.Header.Market}</li>
+                    <li>{translations.Header.Support}</li>
                 </Ul>
-                <LanguageButton>
+                <LanguageButton onClick={() => changeLanguage(language === 'en' ? 'pt' : 'en')}>
                     <img src={LanguageIcon}/>
+                    <p>{language === 'en' ? 'eng' : 'pt'}</p>
                 </LanguageButton>
             </Header>
         </>
