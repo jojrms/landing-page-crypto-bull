bb84867c025eaec6571f26c61cf50f6cfc97f672
Jordana <jordanarmos@gmail.com>
2023-07-11 16:21:19 -0300

    feat: Applying categories in NFT component

--- src/components/pages/NFTCategories.jsx
+++ src/components/pages/NFTCategories.jsx
@@ -2,13 +2,50 @@ import { useContext } from 'react';
 import { LanguageContext } from '../../language.context.js';
 
 import {
-    Background
+    Background,
+    Title,
+    Input,
+    NFTCategoriesGrid,
+    NTFCategory
 } from '../styles/NFTCategories.style.js'
+
+import All from '../images/NFTCategories/All.svg';
+import Games from '../images/NFTCategories/Games.svg';
+import Art from '../images/NFTCategories/Art.svg';
+import Collectibles from '../images/NFTCategories/Collectibles.svg';
+import Music from '../images/NFTCategories/Music.svg';
+import Photography from '../images/NFTCategories/Photography.svg';
+import Domain from '../images/NFTCategories/Domain.svg';
+import Rare from '../images/NFTCategories/Rare.svg';
 
 export default function NFTCategories(){
 
+    const { translations, language, changeLanguage } = useContext(LanguageContext);
+
+    const NFTCategory = [
+        {name: "All NFT", spanColor: "#B621FE", imageUrl: All},
+        {name: "Games", spanColor: "#0BADD1", imageUrl: Games},
+        {name: "Art", spanColor: "#F1C712", imageUrl: Art},
+        {name: "Collectibles", spanColor: "#55D847", imageUrl: Collectibles},
+        {name: "Music", spanColor: "#FE5F75", imageUrl: Music},
+        {name: "Photography", spanColor: "#FC9842", imageUrl: Photography},
+        {name: "Domain Names", spanColor: "#80DEEE", imageUrl: Domain},
+        {name: "Rare NFTs", spanColor: "#F35917", imageUrl: Rare},
+    ]
+
     return(
         <Background>
+            <Title>{translations.NFTCategories.Title}</Title>
+            <Input type='search' placeholder={translations.NFTCategories.InputPlaceholder}/>
+            <NFTCategoriesGrid>
+                {NFTCategory.map( category => {
+                    return <NTFCategory>
+                        <span style={{backgroundColor: category.spanColor}}>
+                            <img src={category.imageUrl}/>
+                        </span>
+                        {category.name}</NTFCategory>
+                })}
+            </NFTCategoriesGrid>
             
         </Background>
     )
--- src/components/styles/NFTCategories.style.js
+++ src/components/styles/NFTCategories.style.js
@@ -5,4 +5,60 @@ export const Background = styled.section`
     min-height: 100px;
     display: grid;
     align-items: center;
+    justify-content: center;
+    padding: 50px 30px;
+    gap: 30px;
+`
+
+export const Title = styled.h1`
+    font-weight: 500;
+    font-size: 2rem;
+    text-align: center;
+`
+
+export const Input = styled.input`
+    width: 45vw;
+    height: 40px;
+    border: none;
+    border-radius: 15px;
+    background-color: #fff;
+    padding: 5px 15px;
+    
+    &:placeholder{
+        color: #BABABA;
+    }
+`
+
+export const NFTCategoriesGrid = styled.div`
+    width: 100%;
+    min-height: 100px;
+    display: grid;
+    gap: 10px;
+    grid-template-columns: repeat( 4, auto);
+    justify-items: center;
+`
+export const NTFCategory = styled.span`
+    width: fit-content;
+    background-color: #202020;
+    padding: 3px 20px 3px 10px;
+    border-radius: 30px;
+    align-items: center;
+    display: grid;
+    grid-template-columns: min-content auto;
+    gap: 15px;
+    justify-items: center;
+    font-size: 16px;
+
+    span{
+        width: 30px;
+        height: 30px;
+        border-radius: 50px;
+        display: grid;
+        align-items: center;
+        justify-items: center;
+
+        img{
+            width: 60%;
+        }
+    }
 `
