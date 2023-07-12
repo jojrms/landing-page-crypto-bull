e090bb5e819c2916c29add7f9a49cd02a9ff38a3
Jordana <jordanarmos@gmail.com>
2023-07-11 21:49:05 -0300

    feat: Ending Collections component

--- src/components/pages/Collections.jsx
+++ src/components/pages/Collections.jsx
@@ -3,16 +3,49 @@ import { LanguageContext } from '../../language.context.js';
 
 import {
     Background,
-    TextBackground
+    TextBackground,
+    NewsText,
+    DigitalText,
+    CollectionsText,
+    DataText,
+    NFTsImages
 } from '../styles/Collections.style.js';
+import NFTCard from './elements/NFTCard.jsx';
+
+import Bull1 from '../images/Collections/Bull1.jpg';
+import Bull2 from '../images/Collections/Bull2.jpg';
+import Bull3 from '../images/Collections/Bull3.jpg';
+
+import User1 from '../images/Collections/username1.png';
+import User2 from '../images/Collections/username2.png';
+import User3 from '../images/Collections/username3.png';
 
 export default function Collections(){
 
     const { translations, language, changeLanguage } = useContext(LanguageContext);
 
+    const CryptoBullList = [
+        {id: 5240, price: "0.0037", userPhoto: User1, username: "IP3356", imageUrl: Bull1},
+        {id: 2348, price: "0.002", userPhoto: User2, username: "Droid74", imageUrl: Bull2},
+        {id: 7443, price: "0.059", userPhoto: User3, username: "MasterPhi", imageUrl: Bull3},
+    ]
+
     return(
         <Background>
+            <TextBackground>
+                <NewsText>{translations.Collections.News}</NewsText>
+                <DigitalText>{translations.Collections.Digital}</DigitalText>
+                <CollectionsText>{translations.Collections.NFTsCollections}</CollectionsText>
+                <DataText>{translations.Collections.A2023Sept28}</DataText>
+            </TextBackground>
 
+            <NFTsImages>
+                {
+                    CryptoBullList.map( element => {
+                        return <NFTCard id={element.id} image={element.imageUrl} price={element.price} userPhoto={element.userPhoto} username={element.username}/>
+                    })
+                }
+            </NFTsImages>
         </Background>
     )
 }
--- src/components/styles/Collections.style.js
+++ src/components/styles/Collections.style.js
@@ -2,17 +2,71 @@ import styled from 'styled-components';
 
 export const Background = styled.section`
     width: 100%;
-    margin: auto;
     min-height: 100px;
     display: grid;
-    align-items: center;
-    justify-items: center;
+    grid-template-columns: 45% auto;
     padding: 100px 30px;
     gap: 30px;
     background-color: #0E0E0E;
+    overflow-x: hidden;
 `
 
 export const TextBackground = styled.div`
+    height: fit-content;
     display: grid;
-    grid-template-colums: auto;
+    grid-template-columns: min-content;
+    column-gap: 10px;
+    grid-template-areas:
+    "news digital"
+    "collections collections"
+    "data data";
+    font-size: 2.3rem;
+    line-height: 55px;
+    margin: auto;
+`
+
+export const NewsText = styled.h1`
+    grid-area: news;
+    background: linear-gradient(to right, #80FF72, #7EE8FA);
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+    text-transform: uppercase;
+    
+`
+export const DigitalText = styled.h1`
+    grid-area: digital;
+    background: linear-gradient(to right, #FE5F75, #FC9842);
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+    text-transform: uppercase;
+`
+export const CollectionsText = styled.h1`
+    grid-area: collections;
+    background: linear-gradient(to right, #B621FE, #8857FC, #1FD1F9);
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+    text-transform: uppercase;
+`
+export const DataText = styled.h1`
+    grid-area: data;
+    background: linear-gradient(to right, #F53803, #F5D020);
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+    text-transform: uppercase;
+`
+
+export const NFTsImages = styled.div`
+    width: initial;
+    margin-left: auto;
+    margin-right: -30px;
+    height: 300px;
+    display: grid;
+    grid-template-columns: repeat(3,  minmax(200px, 250px));
+    gap: 20px;
+    overflow-x: scroll;
+    padding-right: 20px;
+
+    &::-webkit-scrollbar{
+        display: none;
+    }
 `
