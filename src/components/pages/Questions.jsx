680ba25d323b8339311001bda6536084085d55b3
Jordana <jordanarmos@gmail.com>
2023-07-11 23:48:02 -0300

    feat: Making Question component

--- src/components/pages/Questions.jsx
+++ src/components/pages/Questions.jsx
@@ -1,12 +1,16 @@
 import { useContext } from 'react';
 import {
-    Background
-} from '../styles/Questions.style';
-
+    Title,
+} from '../styles/BrowsePossibilities.style';
 import {
-    Title
-} from '../styles/Presentation.style';
+    Background,
+    QuestionGrid,
+    QuestionBackground,
+    QuestionAnswer
+} from '../styles/Questions.style';
 import { LanguageContext } from '../../language.context.js';
+
+import Arrow from '../images/Questions/Arrow.svg';
 
 export default function Questions(){
 
@@ -15,6 +19,17 @@ export default function Questions(){
     return(
         <Background>
             <Title>{translations.Questions.Title}</Title>
+
+            <>
+                <QuestionGrid>
+                    <QuestionBackground>
+                        Lectus non id ullamcorper etiam at orci pharetra, lacus, tortor, velit eu?
+                        <img src={Arrow}/>
+                    </QuestionBackground>
+                    <QuestionAnswer>At pretium, velit interdum vivamus cursus in urna, mattis tristique vitae nibh felis ligula velit, vestibulum, aenean enim imperdiet nibh semper laoreet odio pellentesque massa aliquam interdum pharetra neque, vestibulum elit et</QuestionAnswer>
+                </QuestionGrid>
+            </>
+            
         </Background>
     )
 }
--- src/components/styles/Questions.style.js
+++ src/components/styles/Questions.style.js
@@ -5,9 +5,37 @@ export const Background = styled.section`
     width: 100%;
     min-height: 100px;
     display: grid;
-    padding: 0px 30px;
+    padding: 100px 30px;
     align-items: center;
     justify-items: center;
     background-image: url(${Blur});
     background-size: cover;
+`
+
+export const QuestionGrid = styled.div`
+    width: 70%;
+    display: grid;
+    gap: 10px;
+    margin-top: 15px;
+`
+export const QuestionBackground = styled.span`
+    background-color: #181818;
+    color: #4B4B4B;
+    padding: 25px 50px;
+    border-radius: 10px;
+    display: flex;
+    align-items: center;
+
+    img{
+        width: 15px;
+        height: 15px;
+        padding: 10px;
+        cursor: pointer;
+        margin-left: auto;
+    }
+`
+export const QuestionAnswer = styled.span`
+    border: 1px solid #232323;
+    padding: 25px 50px;
+    border-radius: 10px;
 `
